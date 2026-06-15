4df91989530ef179b6a34911cfa084cae2b80263
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-14 22:29:31 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,23 @@
 const CONFIG = {
   URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbxjM9xKHTPUDCyuXOY8VWcA3NJlSXz7BJkB_-iOyCmOk9nG8RHBq3nI7GlXOesXJ7o3/exec",
   APP_NAME: "INSPECCIONES",
-  APPSHEET_APP_NAME: "APP_INSPECCIONES-236810661",
-  MAPAS_SECTOR: {
-    "EL PROGRESO": {
-      nombre: "Mapa base El Progreso",
-      mid: "1AD2wEag68h4hiBy5R7fbW9Ir-6BmsCs",
-      lat: 15.069569258342678,
-      lng: -86.7047245,
-      zoom: 9
-    },
-    "JUTICALPA": {
-      nombre: "Mapa base Juticalpa",
-      mid: "1AD2wEag68h4hiBy5R7fbW9Ir-6BmsCs",
-      lat: 15.069569258342678,
-      lng: -86.7047245,
-      zoom: 9
-    }
+  APPSHEET_APP_NAME: "APP_INSPECCIONES-236810661"
+};
+
+window.MAPAS_SECTOR = {
+  "JUTICALPA": {
+    nombre: "Mapa base Juticalpa",
+    mid: "1AD2wEag68h4hiBy5R7fbW9Ir-6BmsCs",
+    lat: 14.958349515262615,
+    lng: -86.14916699999998,
+    zoom: 9
+  },
+
+  "EL PROGRESO": {
+    nombre: "Mapa base El Progreso",
+    mid: "1g7RknupgCVrUFtKSJwfEUbqaQK8b-r8",
+    lat: 15.276242326616291,
+    lng: -87.43169250000001,
+    zoom: 9
   }
 };
